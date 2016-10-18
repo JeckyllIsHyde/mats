d76b8ff9a7477abb5479def45eba9e70f96793b5
Jaime Andres Castillo Leon <jacastillol@gmail.com>
2016-10-18 12:25:02 -0500

    Mapa mental del problema

---
+++ calificacion/propuesta/problema.mm
@@ -0,0 +1,137 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Planteamiento del problema Locomocion humanoide" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1473178158999" STYLE="bubble" FORMAT="STANDARD_FORMAT" MAX_WIDTH="200">
+<font SIZE="12" BOLD="true"/>
+<hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
+<node TEXT="Sobre una estructura cognitiva de desarrollo locomotor humanoide" POSITION="right" ID="ID_56697045" CREATED="1473173993861" MODIFIED="1473178471163" MAX_WIDTH="200">
+<edge COLOR="#971b2d"/>
+<node TEXT="construir un lazo sensoriomotriz que represente su morfologia" ID="ID_575135884" CREATED="1473172636753" MODIFIED="1473180177300" MAX_WIDTH="200">
+<node TEXT="Usando el modelo humanoide" ID="ID_1772211097" CREATED="1473172708624" MODIFIED="1473174592328" MAX_WIDTH="200">
+<node TEXT="Musculo de Hill" ID="ID_1825987313" CREATED="1473172681930" MODIFIED="1473172721451"/>
+<node TEXT="Neuro musculo esquletico de Geyer" ID="ID_1219164448" CREATED="1473172721967" MODIFIED="1473174635336" MAX_WIDTH="200">
+<node TEXT="hopping" ID="ID_377555493" CREATED="1473181124130" MODIFIED="1473181127166"/>
+<node TEXT="planar biped" ID="ID_583418887" CREATED="1473181127899" MODIFIED="1473181134587"/>
+<node TEXT="spatial biped" ID="ID_700907896" CREATED="1473181135414" MODIFIED="1473181139824"/>
+</node>
+</node>
+<node TEXT="proponer un canal de informacion basado en" ID="ID_1111915813" CREATED="1473172907024" MODIFIED="1473174549304" MAX_WIDTH="200">
+<node TEXT="Spindles de elongacion, velocidad" ID="ID_1573914349" CREATED="1473172942042" MODIFIED="1473174642593" MAX_WIDTH="200"/>
+<node TEXT="para fuerza Golgi" ID="ID_1113256908" CREATED="1473172961280" MODIFIED="1473172976939"/>
+<node TEXT="Implementar un sistetema recurrente como en Hauser y Maas" ID="ID_1237417765" CREATED="1473173048429" MODIFIED="1473174502796" MAX_WIDTH="200"/>
+<node TEXT="Para formar una red bayesiana del lazo sensoriomotriz" ID="ID_1697815702" CREATED="1473173127120" MODIFIED="1473174511849" MAX_WIDTH="200"/>
+</node>
+<node TEXT="que permita" ID="ID_1069580307" CREATED="1473173519668" MODIFIED="1473173523959">
+<node TEXT="evaluar la computacion morfologica" ID="ID_1493170548" CREATED="1473173535548" MODIFIED="1473174543153" MAX_WIDTH="200">
+<node TEXT="para simplificar el espacio de control" ID="ID_120759852" CREATED="1473173669080" MODIFIED="1473174519682" MAX_WIDTH="200"/>
+<node TEXT="explotar y exporar la dinamica natural" ID="ID_1675641038" CREATED="1473174202038" MODIFIED="1473174526246" MAX_WIDTH="200"/>
+</node>
+<node TEXT="estudiar la formacion de movimientos" ID="ID_1099816868" CREATED="1473173167643" MODIFIED="1473174535160" MAX_WIDTH="200">
+<node TEXT="reflexivos" ID="ID_1063533723" CREATED="1473173199587" MODIFIED="1473173205057"/>
+<node TEXT="coordinados" ID="ID_72217746" CREATED="1473173205573" MODIFIED="1473173211358">
+<node TEXT="discretos" ID="ID_1771842089" CREATED="1473173309875" MODIFIED="1473173322641"/>
+<node TEXT="repetitivos" ID="ID_1131682031" CREATED="1473173306199" MODIFIED="1473173309497"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="generar comportamientos coordinados" ID="ID_859881722" CREATED="1473174012475" MODIFIED="1473178533922" MAX_WIDTH="200">
+<node TEXT="por" ID="ID_1389678556" CREATED="1473177945750" MODIFIED="1473177950331">
+<node TEXT="refuerzo" ID="ID_163106170" CREATED="1473174065271" MODIFIED="1473174438112">
+<node TEXT="empleando" ID="ID_767445087" CREATED="1473178612484" MODIFIED="1473178616427">
+<node TEXT="eNAC" ID="ID_82023276" CREATED="1473178618249" MODIFIED="1473178623832"/>
+<node TEXT="PoWer" ID="ID_1427934166" CREATED="1473178624379" MODIFIED="1473178630795"/>
+<node TEXT="PI&#xb2;" ID="ID_252737074" CREATED="1473178666475" MODIFIED="1473178677091"/>
+</node>
+</node>
+<node TEXT="autoorganizacion" ID="ID_86857572" CREATED="1473178568097" MODIFIED="1473178575142">
+<node TEXT="del estimulo sensorial" ID="ID_1581822210" CREATED="1473178576713" MODIFIED="1473178583039">
+<node TEXT="iida" ID="ID_1534221765" CREATED="1473178586277" MODIFIED="1473178588642"/>
+<node TEXT="playful machines" ID="ID_459326098" CREATED="1473178588969" MODIFIED="1473178596229"/>
+</node>
+</node>
+<node TEXT="demostracion" ID="ID_757131348" CREATED="1473174438739" MODIFIED="1473174456305">
+<node TEXT="usando" ID="ID_10239331" CREATED="1473178642984" MODIFIED="1473178647395">
+<node TEXT="Inverse RL" ID="ID_1288306613" CREATED="1473178649091" MODIFIED="1473178665006"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="partiendo de sistemas libres de modelo" ID="ID_1391057728" CREATED="1473176997574" MODIFIED="1473177884439" MAX_WIDTH="200">
+<node TEXT="obteniendo" ID="ID_1316184637" CREATED="1473177765890" MODIFIED="1473180074636">
+<node TEXT="soluones basadoas en objetivos" ID="ID_71064123" CREATED="1473177023400" MODIFIED="1473180056933" MAX_WIDTH="200" VSHIFT="50"/>
+<node TEXT="codificacion de objetivos en funciones de recompenza" ID="ID_874815134" CREATED="1473177065931" MODIFIED="1473180063624" MAX_WIDTH="200" VSHIFT="10"/>
+<node TEXT="aprendizaje de modelos internos" ID="ID_1643827360" CREATED="1473177085965" MODIFIED="1473180074635" MAX_WIDTH="200" HGAP="30" VSHIFT="-20">
+<node TEXT="predictivos codificados en" ID="ID_1475958053" CREATED="1473177169658" MODIFIED="1473177354513">
+<node TEXT="ASMs" ID="ID_1628497390" CREATED="1473179703956" MODIFIED="1473179708680"/>
+<node TEXT="cpgs" ID="ID_130476791" CREATED="1473177354518" MODIFIED="1473177357587"/>
+<node TEXT="dmp" ID="ID_666034354" CREATED="1473177358126" MODIFIED="1473177361713"/>
+<node TEXT="pmp" ID="ID_156993588" CREATED="1476707268581" MODIFIED="1476707273567"/>
+<node TEXT="otras ANNs" ID="ID_1655084114" CREATED="1473177417584" MODIFIED="1473177428624">
+<node TEXT="Deep NN y LSTM" ID="ID_1564124143" CREATED="1473179566126" MODIFIED="1473179580573"/>
+<node TEXT="reservoirs" ID="ID_761693815" CREATED="1473179581688" MODIFIED="1473179590850"/>
+</node>
+</node>
+<node TEXT="intrisecos" ID="ID_1181368558" CREATED="1473177185742" MODIFIED="1473177189031"/>
+</node>
+</node>
+</node>
+<node TEXT="que resulten en" ID="ID_1995682906" CREATED="1473177232975" MODIFIED="1473177281704" MAX_WIDTH="200">
+<node TEXT="incremento de la velocidad de aprendizaje" ID="ID_1392922506" CREATED="1473177251926" MODIFIED="1473177291497" MAX_WIDTH="200"/>
+<node TEXT="reduccion o simplificacion de los espacios de busqueda" ID="ID_178095871" CREATED="1473177261402" MODIFIED="1473177299325" MAX_WIDTH="200"/>
+<node TEXT="comportamientos" ID="ID_649035754" CREATED="1473179759482" MODIFIED="1473179764015">
+<node TEXT="autonomos" ID="ID_1047460319" CREATED="1473177272608" MODIFIED="1473179775540"/>
+<node TEXT="dirigidos a objetivos" ID="ID_597560499" CREATED="1473179733350" MODIFIED="1473179781281"/>
+</node>
+</node>
+</node>
+</node>
+</map>
