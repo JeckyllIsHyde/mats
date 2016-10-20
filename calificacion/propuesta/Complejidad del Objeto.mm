f3b3e56de4080ad35ba35f7f8ae8dbb83eca7aaf
Jaime Andres <jacastillol@gmail.com>
2016-10-19 20:34:50 -0500

    revision pregunta

--- calificacion/propuesta/Complejidad del Objeto.mm
+++ calificacion/propuesta/Complejidad del Objeto.mm
@@ -1,8 +1,8 @@
 <map version="freeplane 1.3.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="Complejidad del Objeto \textbf{LOCOMOCION HUMANOIDE}" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1474370077739" MAX_WIDTH="250" BACKGROUND_COLOR="#fefead" STYLE="bubble" FORMAT="latexPatternFormat">
+<node TEXT="Complejidad del Objeto \textbf{LOCOMOCION HUMANOIDE}" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1476915352563" MAX_WIDTH="250" BACKGROUND_COLOR="#fefead" STYLE="bubble" FORMAT="latexPatternFormat">
 <font SIZE="12" BOLD="true"/>
-<hook NAME="MapStyle">
+<hook NAME="MapStyle" zoom="0.25">
     <properties show_icon_for_attributes="true"/>
 
 <map_styles>
@@ -52,23 +52,9 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="1"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="8"/>
 <edge STYLE="sharp_bezier" WIDTH="4"/>
-<node TEXT="Problemas Transversales" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1865986939" CREATED="1474316915144" MODIFIED="1474370077739" HGAP="530" VSHIFT="-390" FORMAT="latexPatternFormat">
-<hook NAME="FreeNode"/>
-<edge COLOR="#7c0000"/>
-<cloud COLOR="#fde29d" SHAPE="ARC"/>
-<node TEXT="Robustez" ID="ID_1330626309" CREATED="1474317596694" MODIFIED="1476705925623" FORMAT="latexPatternFormat">
-<edge STYLE="sharp_bezier"/>
-</node>
-<node TEXT="Versatilidad" ID="ID_1462898852" CREATED="1474317600806" MODIFIED="1476705925624" FORMAT="latexPatternFormat">
-<edge STYLE="sharp_bezier"/>
-</node>
-<node TEXT="Eficiencia" ID="ID_1023689288" CREATED="1474317607016" MODIFIED="1476705925625" FORMAT="latexPatternFormat">
-<edge STYLE="sharp_bezier"/>
-</node>
-</node>
-<node TEXT="\textbf{Humaniodes}" POSITION="right" ID="ID_1653187810" CREATED="1474313504240" MODIFIED="1474334502635" FORMAT="latexPatternFormat">
+<node TEXT="\textbf{Humaniodes}" POSITION="right" ID="ID_1653187810" CREATED="1474313504240" MODIFIED="1476915234335" FORMAT="latexPatternFormat" HGAP="60" VSHIFT="80">
 <font SIZE="12" BOLD="true"/>
 <edge COLOR="#15d2d2"/>
 <cloud COLOR="#e0edef" SHAPE="ARC"/>
@@ -83,12 +69,21 @@
 </node>
 <node TEXT="Generacion de Trayectorias" ID="ID_1481521827" CREATED="1474313540522" MODIFIED="1474317764932" FORMAT="latexPatternFormat"/>
 </node>
-<node TEXT="Problemas Transversales" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1912897496" CREATED="1474317054693" MODIFIED="1474369912609" HGAP="560" VSHIFT="-240" FORMAT="latexPatternFormat">
-<edge COLOR="#00007c"/>
+<node TEXT="Problemas Transversales" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1865986939" CREATED="1474316915144" MODIFIED="1476915277008" HGAP="570" VSHIFT="-390" FORMAT="latexPatternFormat">
 <hook NAME="FreeNode"/>
-<cloud COLOR="#fde09d" SHAPE="ARC"/>
+<edge COLOR="#7c0000"/>
+<cloud COLOR="#fde29d" SHAPE="ARC"/>
+<node TEXT="Robustez" ID="ID_1330626309" CREATED="1474317596694" MODIFIED="1476705925623" FORMAT="latexPatternFormat">
+<edge STYLE="sharp_bezier"/>
 </node>
-<node TEXT="\textbf{Biomec&#xe1;nica}" POSITION="right" ID="ID_199834689" CREATED="1474313409235" MODIFIED="1474334813462" FORMAT="latexPatternFormat">
+<node TEXT="Versatilidad" ID="ID_1462898852" CREATED="1474317600806" MODIFIED="1476705925624" FORMAT="latexPatternFormat">
+<edge STYLE="sharp_bezier"/>
+</node>
+<node TEXT="Eficiencia" ID="ID_1023689288" CREATED="1474317607016" MODIFIED="1476705925625" FORMAT="latexPatternFormat">
+<edge STYLE="sharp_bezier"/>
+</node>
+</node>
+<node TEXT="\textbf{Biomec&#xe1;nica}" POSITION="right" ID="ID_199834689" CREATED="1474313409235" MODIFIED="1476915243400" FORMAT="latexPatternFormat" HGAP="160" VSHIFT="-40">
 <font SIZE="12" BOLD="true"/>
 <edge COLOR="#ff0000"/>
 <cloud COLOR="#f2c6c7" SHAPE="ARC"/>
@@ -100,12 +95,18 @@
 <node TEXT="Sistema M&#xfa;sculo-esquel&#xe9;tico" ID="ID_1588830836" CREATED="1474313647498" MODIFIED="1474317775720" FORMAT="latexPatternFormat"/>
 <node TEXT="Protesis" ID="ID_1841887839" CREATED="1474313628634" MODIFIED="1474317775720" FORMAT="latexPatternFormat"/>
 </node>
-<node TEXT="Problemas Transversales" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_80973098" CREATED="1474328368779" MODIFIED="1474334386477" FORMAT="latexPatternFormat" HGAP="610" VSHIFT="-50">
-<edge COLOR="#007c00"/>
+<node TEXT="Problemas Transversales" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_1912897496" CREATED="1474317054693" MODIFIED="1476915280898" HGAP="680" VSHIFT="-180" FORMAT="latexPatternFormat">
+<edge COLOR="#00007c"/>
 <hook NAME="FreeNode"/>
 <cloud COLOR="#fde09d" SHAPE="ARC"/>
+<node TEXT="Eficiencia" ID="ID_1500619106" CREATED="1476914725842" MODIFIED="1476914965446" FORMAT="latexPatternFormat">
+<edge STYLE="sharp_bezier"/>
 </node>
-<node TEXT="\textbf{Neuromec&#xe1;nica}" POSITION="right" ID="ID_1605409240" CREATED="1474313418261" MODIFIED="1474317820484" FORMAT="latexPatternFormat">
+<node TEXT="Sinergia muscular" ID="ID_865061586" CREATED="1476914736031" MODIFIED="1476914965449" FORMAT="latexPatternFormat">
+<edge STYLE="sharp_bezier"/>
+</node>
+</node>
+<node TEXT="\textbf{Neuromec&#xe1;nica}" POSITION="right" ID="ID_1605409240" CREATED="1474313418261" MODIFIED="1476915271152" FORMAT="latexPatternFormat" HGAP="260" VSHIFT="20">
 <font SIZE="12" BOLD="true"/>
 <edge COLOR="#0000ff"/>
 <cloud COLOR="#ccc9e9" SHAPE="ARC"/>
@@ -120,25 +121,38 @@
 <node TEXT="Neuro-protesis" ID="ID_471518218" CREATED="1474313694603" MODIFIED="1474317784310" FORMAT="latexPatternFormat"/>
 <node TEXT="Parametrizacion de modelos" ID="ID_497430481" CREATED="1474313822359" MODIFIED="1474317784310" FORMAT="latexPatternFormat"/>
 </node>
-<node TEXT="\textbf{Control &#xd3;ptimo Estoc&#xe1;stico}" POSITION="right" ID="ID_973063881" CREATED="1474333443851" MODIFIED="1474334843351" FORMAT="latexPatternFormat">
+<node TEXT="Problemas Transversales" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_80973098" CREATED="1474328368779" MODIFIED="1476915287211" FORMAT="latexPatternFormat" HGAP="850" VSHIFT="50">
+<edge COLOR="#007c00"/>
+<hook NAME="FreeNode"/>
+<cloud COLOR="#fde09d" SHAPE="ARC"/>
+<node TEXT="Pathways" ID="ID_1339631989" CREATED="1476914898809" MODIFIED="1476914979398" FORMAT="latexPatternFormat">
+<edge STYLE="sharp_bezier"/>
+</node>
+<node TEXT="Parametrizaci&#xf3;n" ID="ID_558153471" CREATED="1476914913218" MODIFIED="1476914979402" FORMAT="latexPatternFormat">
+<edge STYLE="sharp_bezier"/>
+</node>
+</node>
+<node TEXT="\textbf{Control &#xd3;ptimo Estoc&#xe1;stico}" POSITION="right" ID="ID_973063881" CREATED="1474333443851" MODIFIED="1476915129392" FORMAT="latexPatternFormat" HGAP="620" VSHIFT="80">
 <font SIZE="12"/>
 <edge COLOR="#7c007c"/>
 <cloud COLOR="#f7b8ff" SHAPE="ARC"/>
-<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="4" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="TeX Gyre Pagella Math" DESTINATION="ID_1397381308" MIDDLE_LABEL="Programacion din&#xe1;mica" STARTINCLINATION="318;-70;" ENDINCLINATION="546;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#c104e4" WIDTH="6" TRANSPARENCY="100" FONT_SIZE="11" FONT_FAMILY="TeX Gyre Pagella Math" DESTINATION="ID_1397381308" MIDDLE_LABEL="Programacion din&#xe1;mica" STARTINCLINATION="318;-70;" ENDINCLINATION="546;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
 </node>
-<node TEXT="Problemas Transversales" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_68598834" CREATED="1474334094856" MODIFIED="1474334408418" HGAP="800" VSHIFT="330" FORMAT="latexPatternFormat">
-<edge COLOR="#7c7c00"/>
-<hook NAME="FreeNode"/>
-<cloud COLOR="#fde09d" SHAPE="ARC"/>
+<node TEXT="\textbf{Inteligencia Artificial}" POSITION="right" ID="ID_591969353" CREATED="1476889439491" MODIFIED="1476915621030" BACKGROUND_COLOR="#acbefd" FORMAT="latexPatternFormat" HGAP="300" VSHIFT="-40">
+<edge COLOR="#00007c"/>
+<cloud COLOR="#acbefd" SHAPE="ARC"/>
+<font SIZE="12"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#105fc2" WIDTH="6" TRANSPARENCY="100" FONT_SIZE="11" FONT_FAMILY="TeXGyrePagella" DESTINATION="ID_1664712043" MIDDLE_LABEL="Aprendizaje de M&#xe1;quina" STARTINCLINATION="-151;118;" ENDINCLINATION="-89;-72;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
 </node>
-<node TEXT="\textbf{Neurociencias}" POSITION="right" ID="ID_1748127526" CREATED="1474313434917" MODIFIED="1474328336126" FORMAT="latexPatternFormat">
+<node TEXT="\textbf{Neurociencias}" POSITION="right" ID="ID_1748127526" CREATED="1474313434917" MODIFIED="1476915310639" FORMAT="latexPatternFormat" HGAP="260">
 <font SIZE="12" BOLD="false"/>
 <edge COLOR="#0e910e"/>
 <cloud COLOR="#c0ead0" SHAPE="ARC"/>
 <node TEXT="Cognicion" ID="ID_305266885" CREATED="1474314154397" MODIFIED="1474317805880" FORMAT="latexPatternFormat">
 <font BOLD="false"/>
-<node TEXT="Aprendizaje" ID="ID_1664712043" CREATED="1474314150508" MODIFIED="1474317805880" FORMAT="latexPatternFormat">
+<node TEXT="Aprendizaje" ID="ID_1664712043" CREATED="1474314150508" MODIFIED="1476913034684" BACKGROUND_COLOR="#acbefd" FORMAT="latexPatternFormat">
 <font BOLD="false"/>
+<cloud COLOR="#acbefd" SHAPE="ARC"/>
 <node TEXT="Supervisado" ID="ID_642076559" CREATED="1474315195333" MODIFIED="1474317805881" FORMAT="latexPatternFormat">
 <font BOLD="false"/>
 </node>
@@ -161,7 +175,7 @@
 </node>
 </node>
 </node>
-<node TEXT="Psicologia" ID="ID_1819111182" CREATED="1474327514893" MODIFIED="1474327569695" FORMAT="latexPatternFormat">
+<node TEXT="Psicologia" ID="ID_1819111182" CREATED="1474327514893" MODIFIED="1476889288333" FORMAT="latexPatternFormat">
 <node TEXT="Motivacion" ID="ID_478117220" CREATED="1474314267113" MODIFIED="1474317805882" FORMAT="latexPatternFormat">
 <font BOLD="false"/>
 </node>
@@ -195,12 +209,28 @@
 </node>
 </node>
 </node>
-<node TEXT="Problemas longitudinales" POSITION="right" ID="ID_1585979251" CREATED="1474334502634" MODIFIED="1474369792641" HGAP="30" VSHIFT="20" FORMAT="latexPatternFormat">
+<node TEXT="Problemas longitudinales" POSITION="right" ID="ID_1585979251" CREATED="1474334502634" MODIFIED="1476916248507" HGAP="248" VSHIFT="20" FORMAT="latexPatternFormat">
 <font SIZE="14"/>
 <edge COLOR="#010101"/>
 <hook NAME="SummaryNode"/>
-<node TEXT="Integracion" ID="ID_535610201" CREATED="1474334605987" MODIFIED="1474369792640" FORMAT="latexPatternFormat" VSHIFT="-10"/>
-<node TEXT="Implementacion" ID="ID_893845549" CREATED="1474369697404" MODIFIED="1474369810485" FORMAT="latexPatternFormat"/>
+<node TEXT="Integraci&#xf3;n" ID="ID_535610201" CREATED="1474334605987" MODIFIED="1476915529622" FORMAT="latexPatternFormat" VSHIFT="-10"/>
+<node TEXT="Implementaci&#xf3;n" ID="ID_893845549" CREATED="1474369697404" MODIFIED="1476915534089" FORMAT="latexPatternFormat"/>
+<node TEXT="Computaci&#xf3;n morfol&#xf3;gica" ID="ID_1715232661" CREATED="1476915481685" MODIFIED="1476915550284" FORMAT="latexPatternFormat" VSHIFT="12"/>
+<node TEXT="Lazo sensoriomotor" ID="ID_570419745" CREATED="1476916215605" MODIFIED="1476916254180" HGAP="21" VSHIFT="12" FORMAT="latexPatternFormat"/>
+</node>
+<node TEXT="Problemas Transversales" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_68598834" CREATED="1474334094856" MODIFIED="1476915352558" HGAP="1050" VSHIFT="650" FORMAT="latexPatternFormat">
+<edge COLOR="#7c7c00"/>
+<hook NAME="FreeNode"/>
+<cloud COLOR="#fde09d" SHAPE="ARC"/>
+<node TEXT="Parametrizaci&#xf3;n" ID="ID_1256299425" CREATED="1476915400804" MODIFIED="1476915457821" FORMAT="latexPatternFormat">
+<edge STYLE="sharp_bezier"/>
+</node>
+<node TEXT="Dimensionalidad" ID="ID_37508521" CREATED="1476915415630" MODIFIED="1476915457819" FORMAT="latexPatternFormat">
+<edge STYLE="sharp_bezier"/>
+</node>
+<node TEXT="Estructura" ID="ID_1945755945" CREATED="1476915430446" MODIFIED="1476915457817" FORMAT="latexPatternFormat">
+<edge STYLE="sharp_bezier"/>
+</node>
 </node>
 </node>
 </map>
