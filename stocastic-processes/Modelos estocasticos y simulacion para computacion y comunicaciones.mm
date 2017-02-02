9ac2f49b2617311070ec6abc36561a6fb618ca3e
Jaime Andres <jacastillol@gmail.com>
2017-02-02 10:09:31 -0500

    propuesta; y clases de vision y estocasticos

---
+++ stocastic-processes/Modelos estocasticos y simulacion para computacion y comunicaciones.mm
@@ -0,0 +1,154 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1486039958154" MAX_WIDTH="300" MIN_WIDTH="300"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Modelos estoc&#225;sticos y simulaci&#243;n para computaci&#243;n y comunicaciones
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Jorge eduardo ortiz trivi&#241;o
+    </p>
+    <p>
+      jeortizt@unal.edu.co
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Oficina 453 - 110
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Lunes y martes 10-11am
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Contenido" POSITION="right" ID="ID_1071072814" CREATED="1485866298663" MODIFIED="1485866306037">
+<edge COLOR="#ff0000"/>
+<node TEXT="1. Introduccion" ID="ID_662123904" CREATED="1485866306057" MODIFIED="1485866312809">
+<node TEXT="Analizar sistemas de computo y comunicacion que contienen:" ID="ID_1798322872" CREATED="1485866426042" MODIFIED="1485866450271">
+<node TEXT="Tiempo espacio" ID="ID_1209482441" CREATED="1485866450288" MODIFIED="1485866455975">
+<node TEXT="$T \in \mathbb{R}^m$" ID="ID_371704918" CREATED="1485866689714" MODIFIED="1485867925195" FORMAT="latexPatternFormat"/>
+</node>
+<node TEXT="Estado" ID="ID_1145362639" CREATED="1485866457048" MODIFIED="1485866459502">
+<node TEXT="$X \in \mathbb{R}^n$ con $x_i:\mathbb{R}^m \to \mathbb{R}, t \to x_i(t)$ con $x_i$ dependientes para todo $i$, X es un vector estocastico. $f_X^t(x)$, es un proceso estocastico" ID="ID_115565557" CREATED="1485866689714" MODIFIED="1485867946597" FORMAT="latexPatternFormat"/>
+<node TEXT="maquina de turing probabilistica" ID="ID_1462065280" CREATED="1485866979252" MODIFIED="1485866992154"/>
+</node>
+<node TEXT="Comportamiento" ID="ID_1892224544" CREATED="1485866460287" MODIFIED="1485866463851">
+<node TEXT="$Y\in\mathbb{R}^p$, $y_k:\mathbb{R}^{n+m}\to\mathbb{R}, (t,x)\to Y_k(t,x)$&#xa;$f_Y(y)$" ID="ID_597665331" CREATED="1485867065932" MODIFIED="1485867902526" FORMAT="latexPatternFormat"/>
+</node>
+<node TEXT="Leyes cientificas" ID="ID_869025139" CREATED="1485866468678" MODIFIED="1485866492232">
+<node TEXT="$\theta\in\mathbb{R}^q$, $\theta_r:\mathbb{R}^{m+n+p}\to\mathbb{R}$, $(t,x,y)\to\theta_r(t,x,y)$," ID="ID_931941827" CREATED="1485867473950" MODIFIED="1485867879440" FORMAT="latexPatternFormat"/>
+</node>
+</node>
+</node>
+<node TEXT="2. Objetivos del curso" ID="ID_1552955893" CREATED="1485866313335" MODIFIED="1485866321295"/>
+<node TEXT="3. Prerequisitos" ID="ID_427844505" CREATED="1485866321849" MODIFIED="1485866328841"/>
+<node TEXT="4. Capitulos" ID="ID_1028205655" CREATED="1485866329288" MODIFIED="1485866337106"/>
+<node TEXT="5. Metodologia" ID="ID_1365716067" CREATED="1485866337633" MODIFIED="1485866342855">
+<node TEXT="Tareas" ID="ID_187482823" CREATED="1486038986907" MODIFIED="1486038990255">
+<node TEXT="obligatorias" ID="ID_357568115" CREATED="1486038993116" MODIFIED="1486038996127"/>
+<node TEXT="opcionales" ID="ID_1910017574" CREATED="1486038990265" MODIFIED="1486038992282"/>
+</node>
+</node>
+<node TEXT="6. Evaluacion" ID="ID_962399648" CREATED="1485866343363" MODIFIED="1485866349450">
+<node TEXT="1. Dos parciales 20%" ID="ID_1688701944" CREATED="1486038744419" MODIFIED="1486039578907"/>
+<node TEXT="2. Expociciones 30%" ID="ID_559276044" CREATED="1486038752231" MODIFIED="1486039575059"/>
+<node TEXT="3. Simulacion en NS-2 (o NS-3) 20%" ID="ID_1411620864" CREATED="1486038796612" MODIFIED="1486039571889"/>
+<node TEXT="4. Proyecto 30%" ID="ID_1409259367" CREATED="1486038814041" MODIFIED="1486039568333"/>
+<node TEXT="5. Puntos adicionales" ID="ID_843464149" CREATED="1486039541234" MODIFIED="1486039561683"/>
+</node>
+<node TEXT="7- Designacion de monitores del curso" ID="ID_1262610927" CREATED="1485866349923" MODIFIED="1485866369076"/>
+<node TEXT="8. Referencias bibliograficas" ID="ID_1937560224" CREATED="1485866370068" MODIFIED="1485866381703">
+<node TEXT="1. Ross, Sheldon M. PROBABILITY MODELS FOR COMPUTER SCIENCE. Harcourt Academic Press 2002" ID="ID_708379530" CREATED="1486041028078" MODIFIED="1486041415796" MAX_WIDTH="400" MIN_WIDTH="400"/>
+<node TEXT="2. Mood, A. M.; Grabill, F. A.; Boes, Duane. INTRODUCTION TO THE THEORY OF STATISTICS. Third Edition. Mc Graw-Hill International Editions. 1974." ID="ID_622259442" CREATED="1486041211240" MODIFIED="1486041415816" MAX_WIDTH="400" MIN_WIDTH="400"/>
+<node TEXT="3. Taha, Hamdy. INVESTIGACION DE OPERACIONES: UNA INTRODUCCION. Sexta Edicion. Pearson  1998." ID="ID_835049180" CREATED="1486041337627" MODIFIED="1486041428942" MAX_WIDTH="400" MIN_WIDTH="400"/>
+<node TEXT="4. Bunge, Mario. La ciencia. Su m&#xe9;todo y su filosof&#xed;a" ID="ID_1876488428" CREATED="1486041600390" MODIFIED="1486042137295" MAX_WIDTH="400" MIN_WIDTH="400">
+<node TEXT="ENSAYO 1. qu&#xe9; es la ciencia" ID="ID_1427758779" CREATED="1486041612831" MODIFIED="1486041752744"/>
+<node TEXT="ENSAYO 2. cu&#xe1;l es el m&#xe9;todo de la ciencia" ID="ID_1439381176" CREATED="1486041619231" MODIFIED="1486041731426"/>
+<node TEXT="ENSAYO 3. qu&#xe9; significa ley cient&#xed;fica" ID="ID_138316466" CREATED="1486041624753" MODIFIED="1486041717725"/>
+<node TEXT="ENSAYO 4. hacer ciencia filosoficamente y filosofar la ciencia" ID="ID_1382093630" CREATED="1486041678680" MODIFIED="1486041702252"/>
+</node>
+</node>
+</node>
+<node TEXT="Tareas" POSITION="right" ID="ID_698109159" CREATED="1486041440816" MODIFIED="1486041446304">
+<edge COLOR="#0000ff"/>
+<node TEXT="1. Ejercicio No. 56, P&#xe1;g 48. de Mood" ID="ID_1355659307" CREATED="1486041446313" MODIFIED="1486043650443">
+<node TEXT="(Juego de craps) soluci&#xf3;n anal&#xed;tica y soluci&#xf3;n simulada" ID="ID_870800427" CREATED="1486041531598" MODIFIED="1486043582073"/>
+</node>
+<node TEXT="2. Opcional Leer un ensayo" ID="ID_1227241656" CREATED="1486043247567" MODIFIED="1486043278111">
+<node TEXT="Hardy, G. H. Apolog&#xed;a de un matem&#xe1;tico" ID="ID_1208196444" CREATED="1486043298698" MODIFIED="1486043527092"/>
+</node>
+</node>
+</node>
+</map>
