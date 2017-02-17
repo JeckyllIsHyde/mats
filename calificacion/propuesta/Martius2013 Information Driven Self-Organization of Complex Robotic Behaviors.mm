905f8cdb9eb57cf6833f1ed6f9451962b7dd4c66
Jaime Andres <jacastillol@gmail.com>
2017-02-17 10:52:09 -0500

    Martius2013 autonomy for self-organization control

---
+++ calificacion/propuesta/Martius2013 Information Driven Self-Organization of Complex Robotic Behaviors.mm
@@ -0,0 +1,188 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1486762775229" MAX_WIDTH="300" MIN_WIDTH="300"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Information Driven Self-Organization of Complex Robotic Behaviors
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      \cite{Martius2013}
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Martius, G., Der, R., &amp; Ay, N. (2013). Information Driven Self-Organization of Complex Robotic Behaviors. PLoS ONE, 8(5). https://doi.org/10.1371/journal.pone.0063400
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Abstract" POSITION="right" ID="ID_420217976" CREATED="1486762779196" MODIFIED="1486762784016">
+<edge COLOR="#ff0000"/>
+<node TEXT="Main ideas" ID="ID_1492672712" CREATED="1486772978494" MODIFIED="1486772988002">
+<node TEXT="Information theory to drive autonomous systems because" ID="ID_778064685" CREATED="1486763928632" MODIFIED="1486764017985" MAX_WIDTH="300" MIN_WIDTH="300">
+<node TEXT="domain invariant" ID="ID_772004863" CREATED="1486763960357" MODIFIED="1486763985522"/>
+<node TEXT="intuitive interpretation" ID="ID_630839717" CREATED="1486763985961" MODIFIED="1486763993691"/>
+</node>
+<node TEXT="The information principle is formulated at the level of behavior, is translated to the dynamics of the synapses" ID="ID_1035362603" CREATED="1486763852133" MODIFIED="1486764028274" MAX_WIDTH="300" MIN_WIDTH="300"/>
+<node TEXT="Predictive Information PI of the sensorimotor process as" ID="ID_55843389" CREATED="1486764073376" MODIFIED="1486764155786" MAX_WIDTH="300" MIN_WIDTH="300">
+<node TEXT="a driving force to generate behavior" ID="ID_1082640353" CREATED="1486764107253" MODIFIED="1486764127545"/>
+</node>
+</node>
+<node TEXT="Results" ID="ID_39116672" CREATED="1486764196556" MODIFIED="1486764202994">
+<node TEXT="spontaneous cooperativity in" ID="ID_118431126" CREATED="1486772467701" MODIFIED="1486772489119">
+<node TEXT="a complex physical system with decentralized control" ID="ID_1301104483" CREATED="1486772489137" MODIFIED="1486772524615"/>
+</node>
+<node TEXT="behavior decomposed in" ID="ID_1862166629" CREATED="1486772283329" MODIFIED="1486772294553">
+<node TEXT="a succesion of low-dimensional modes" ID="ID_1355955715" CREATED="1486772294564" MODIFIED="1486772328467">
+<node TEXT="that increasingly explore the behavior space" ID="ID_1324548932" CREATED="1486772374937" MODIFIED="1486772401142"/>
+</node>
+<node TEXT="promising to avoid course of dimesionality" ID="ID_367692236" CREATED="1486772328999" MODIFIED="1486772357162"/>
+</node>
+<node TEXT="joint controlled humanoid develops a high behavioral variety depending on" ID="ID_1762166756" CREATED="1486772577591" MODIFIED="1486772898172">
+<node TEXT="its physics" ID="ID_678403697" CREATED="1486772898181" MODIFIED="1486772909391"/>
+<node TEXT="environment it is" ID="ID_1414768341" CREATED="1486772909793" MODIFIED="1486772939878">
+<node TEXT="dynamically embedded into" ID="ID_627482507" CREATED="1486772939889" MODIFIED="1486772957663"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Introduction" POSITION="right" ID="ID_738209775" CREATED="1486762784714" MODIFIED="1486762788074">
+<edge COLOR="#0000ff"/>
+<node TEXT="Motivation" ID="ID_991427004" CREATED="1486762884678" MODIFIED="1486762891572">
+<node TEXT="autonomy as" ID="ID_579676229" CREATED="1486773122852" MODIFIED="1486773318978">
+<node TEXT="the ability of independent exploration" ID="ID_119065652" CREATED="1486773318988" MODIFIED="1486773321409"/>
+<node TEXT="the ability to modify its own pattern activity for" ID="ID_1601574096" CREATED="1486773322070" MODIFIED="1486773356948">
+<node TEXT="adaptation in new situations" ID="ID_933985394" CREATED="1486773356958" MODIFIED="1486773379201"/>
+</node>
+<node TEXT="a learning system with novel information" ID="ID_1884921981" CREATED="1486773397120" MODIFIED="1486773445982">
+<node TEXT="improving its cognitive capabilities" ID="ID_1931988597" CREATED="1486773445991" MODIFIED="1486773476310"/>
+<node TEXT="essential for development" ID="ID_1750606519" CREATED="1486773476906" MODIFIED="1486773484926"/>
+</node>
+</node>
+<node TEXT="eficient exploration" ID="ID_361865446" CREATED="1486773494964" MODIFIED="1486773627930">
+<node TEXT="the challenges for high-dimensional spaces" ID="ID_649267355" CREATED="1486773662108" MODIFIED="1486773689705"/>
+<node TEXT="exploration-explotation trade-off in reinforcement learning" ID="ID_701669389" CREATED="1486773696576" MODIFIED="1486773726726"/>
+<node TEXT="solving with" ID="ID_1204675069" CREATED="1486773776353" MODIFIED="1486774082950">
+<node TEXT="Bayesian formulation is intractable" ID="ID_1900824588" CREATED="1486774082961" MODIFIED="1486774085863"/>
+<node TEXT="Intrinsic motivation for focusing search" ID="ID_723867912" CREATED="1486774044826" MODIFIED="1486774330348">
+<node TEXT="on certain things and constraining to smaller space" ID="ID_262918976" CREATED="1486774330358" MODIFIED="1486774399639"/>
+</node>
+<node TEXT="goal-free exploration mechanism" ID="ID_1611626419" CREATED="1486774772516" MODIFIED="1486774789923">
+<node TEXT="of the dynamical properties of a physical system" ID="ID_1222970935" CREATED="1486775975700" MODIFIED="1486776014371"/>
+<node TEXT="self-determined as deterministic function of internal state variables" ID="ID_1212938277" CREATED="1486777591295" MODIFIED="1486777671218"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Relation to other works" ID="ID_1374888645" CREATED="1486762892013" MODIFIED="1486762905675">
+<node TEXT="Information theoretic meassures" ID="ID_242941500" CREATED="1486762908461" MODIFIED="1486762924969"/>
+<node TEXT="Intrinsic motivation" ID="ID_63420603" CREATED="1486762925463" MODIFIED="1486762930682"/>
+<node TEXT="Embodiment" ID="ID_926632484" CREATED="1486762931412" MODIFIED="1486762936817"/>
+<node TEXT="Spontaneity" ID="ID_604357849" CREATED="1486762938328" MODIFIED="1486762954961"/>
+</node>
+</node>
+<node TEXT="Methods" POSITION="right" ID="ID_686128525" CREATED="1486762788876" MODIFIED="1486762813023">
+<edge COLOR="#00ff00"/>
+<node TEXT="Predictive information" ID="ID_1620975863" CREATED="1486762958777" MODIFIED="1486762963896">
+<node TEXT="Nonstationary and Time-local Predictive Information (TiPI)" ID="ID_226641445" CREATED="1486762977685" MODIFIED="1486763170208"/>
+</node>
+<node TEXT="Estimating the TiPI" ID="ID_1015121133" CREATED="1486763171880" MODIFIED="1486763186980">
+<node TEXT="Error propagation dynamics" ID="ID_326094110" CREATED="1486763234293" MODIFIED="1486763277395"/>
+</node>
+<node TEXT="The exporation dynamics" ID="ID_1301443206" CREATED="1486763278195" MODIFIED="1486763287985">
+<node TEXT="Gradient ascending the TiPI" ID="ID_150278475" CREATED="1486763294524" MODIFIED="1486763306450"/>
+<node TEXT="Learning vs exploration dynamics" ID="ID_1079266779" CREATED="1486763331317" MODIFIED="1486763342960"/>
+<node TEXT="One-shot gradients" ID="ID_1249657882" CREATED="1486763344569" MODIFIED="1486763350513"/>
+<node TEXT="Synergy of system and exploration dynamics" ID="ID_1974652968" CREATED="1486763355746" MODIFIED="1486763377888"/>
+<node TEXT="Self-directed search" ID="ID_436605394" CREATED="1486763378390" MODIFIED="1486763385527"/>
+</node>
+<node TEXT="The sensorimotor loop" ID="ID_1010943826" CREATED="1486763392322" MODIFIED="1486763399810">
+<node TEXT="Exploration dynamics for neural control systems" ID="ID_1277103935" CREATED="1486763408105" MODIFIED="1486763420627"/>
+<node TEXT="The Hebbian nature of the update rules" ID="ID_108315242" CREATED="1486763421665" MODIFIED="1486763447750"/>
+</node>
+</node>
+<node TEXT="Results" POSITION="right" ID="ID_1076517368" CREATED="1486762832177" MODIFIED="1486762835394">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Hysteresis Systems" ID="ID_1732054518" CREATED="1486763465565" MODIFIED="1486763478709">
+<node TEXT="Deterministic self-induced hysteresis oscillation" ID="ID_1358960923" CREATED="1486763573621" MODIFIED="1486763592593"/>
+<node TEXT="About time windows" ID="ID_1577545447" CREATED="1486763598103" MODIFIED="1486763603305"/>
+</node>
+<node TEXT="Spontaneous Cooperation with Decentralized Control" ID="ID_661465038" CREATED="1486763651375" MODIFIED="1486763672612">
+<node TEXT="The armband" ID="ID_1071688316" CREATED="1486763682433" MODIFIED="1486763690062"/>
+</node>
+<node TEXT="High Dimensional case - the humanoid" ID="ID_183394423" CREATED="1486763710902" MODIFIED="1486763724606"/>
+</node>
+<node TEXT="Discussion" POSITION="right" ID="ID_1108933663" CREATED="1486762848422" MODIFIED="1486762858368">
+<edge COLOR="#00ffff"/>
+</node>
+</node>
+</map>
