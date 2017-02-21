fdd978d7c0ca55e150427f0baaeae5fad850191b
Jaime Andres <jacastillol@gmail.com>
2017-02-21 09:22:53 -0500

    Clase densidad, distribucion y percentiles. Marques2014

---
+++ calificacion/propuesta/Marques2014 From Spontaneous Motor Activity to Coordinated Behaviour A Developmental Model.mm
@@ -0,0 +1,130 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1487681374147" MAX_WIDTH="300" MIN_WIDTH="300"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      From Spontaneous Motor Activity to Coordinated Behaviour: A Developmental Model
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      \cite{Marques2014}
+    </p>
+    <p>
+      
+    </p>
+    <p style="margin-left: 24pt; text-indent: -24.0pt">
+      Marques, H. G., Bharadwaj, A., &amp; Iida, F. (2014). From Spontaneous Motor Activity to Coordinated Behaviour: A Developmental Model. <i>PLoS Computational Biology</i>, <i>10</i>(7). https://doi.org/10.1371/journal.pcbi.1003653
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Abstract" POSITION="right" ID="ID_744927180" CREATED="1487681376256" MODIFIED="1487681379583">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="Introduction" POSITION="right" ID="ID_1335536666" CREATED="1487681380324" MODIFIED="1487681383084">
+<edge COLOR="#0000ff"/>
+<node TEXT="9 paragraphs" ID="ID_278249192" CREATED="1487682666177" MODIFIED="1487682671224"/>
+</node>
+<node TEXT="Models" POSITION="right" ID="ID_1655563588" CREATED="1487681398596" MODIFIED="1487681401506">
+<edge COLOR="#00ff00"/>
+<node TEXT="2 paragraphs" ID="ID_1589218891" CREATED="1487682719218" MODIFIED="1487682724674"/>
+<node TEXT="Model of musculoskeletal and environment system" ID="ID_1443883017" CREATED="1487681464887" MODIFIED="1487681496430">
+<node TEXT="4 paragraphs" ID="ID_1746782788" CREATED="1487682746213" MODIFIED="1487682750149"/>
+</node>
+<node TEXT="Model of the periferial system" ID="ID_415457451" CREATED="1487681513798" MODIFIED="1487681524101">
+<node TEXT="1 paragraphs" ID="ID_1982920504" CREATED="1487682760894" MODIFIED="1487682764711"/>
+</node>
+<node TEXT="Model of spontaneous motor activity" ID="ID_857803421" CREATED="1487681529883" MODIFIED="1487681540097">
+<node TEXT="3 paragraphs" ID="ID_1690980168" CREATED="1487682771066" MODIFIED="1487682775103"/>
+</node>
+<node TEXT="Model of reflex circuits and supraspinal system" ID="ID_1907840309" CREATED="1487681541043" MODIFIED="1487681568823"/>
+<node TEXT="Experimental procedure" ID="ID_1042998683" CREATED="1487681578838" MODIFIED="1487681584930"/>
+</node>
+<node TEXT="Results" POSITION="right" ID="ID_1564812567" CREATED="1487681592512" MODIFIED="1487681595104">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Self-organization of reflexes from SMA" ID="ID_621063889" CREATED="1487681628344" MODIFIED="1487681643347"/>
+<node TEXT="Coordinated behavior from the modulation of reflexes" ID="ID_901269234" CREATED="1487681679328" MODIFIED="1487681707112"/>
+<node TEXT="Adaptivity to morphological changes" ID="ID_339327135" CREATED="1487681721064" MODIFIED="1487681736251"/>
+<node TEXT="Changing the modulation gains over time" ID="ID_248152820" CREATED="1487681755061" MODIFIED="1487681768605"/>
+<node TEXT="Scalability to point-to-point trajectories" ID="ID_1568242225" CREATED="1487681776508" MODIFIED="1487681797079"/>
+</node>
+<node TEXT="Discussion" POSITION="right" ID="ID_245648079" CREATED="1487681810643" MODIFIED="1487681835865">
+<edge COLOR="#00ffff"/>
+<node TEXT="Interpretation of the obtained reflex networks" ID="ID_1327191497" CREATED="1487681835873" MODIFIED="1487681871170"/>
+<node TEXT="The development of spinal reflexes from SMA" ID="ID_794273154" CREATED="1487681904462" MODIFIED="1487681940251"/>
+<node TEXT="Assumptions and implications of the SMA model" ID="ID_916311583" CREATED="1487681969280" MODIFIED="1487682001643"/>
+<node TEXT="Assumptions and simplifications of the neural circuitry" ID="ID_1783336477" CREATED="1487682002124" MODIFIED="1487682026353"/>
+<node TEXT="Reflexes as possible mechanisms to coordinate behavior" ID="ID_581133846" CREATED="1487682060124" MODIFIED="1487682162757"/>
+<node TEXT="Implications of the mechanical muscle model" ID="ID_1628746653" CREATED="1487682175892" MODIFIED="1487682196451"/>
+<node TEXT="Muscle synergies, modularity, and reduction of dimensionality" ID="ID_1424475095" CREATED="1487682350247" MODIFIED="1487682450700"/>
+<node TEXT="Implications of the developmental model in artifitial systems" ID="ID_238695543" CREATED="1487682507395" MODIFIED="1487682533760"/>
+</node>
+</node>
+</map>
